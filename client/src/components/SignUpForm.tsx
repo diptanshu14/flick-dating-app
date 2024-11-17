c33b45f0c3679a472cc46caaa973a5e1fad4ba27
diptanshu14 <chattopadhyaydiptanshu@gmail.com>
2024-11-17 11:31:46 +0530

    auth pages ui

---
+++ client/src/components/SignUpForm.tsx
@@ -0,0 +1,169 @@
+import { useState } from 'react'
+
+const SignUpForm = () => {
+    const [name, setName] = useState<string>("")
+    const [email, setEmail] = useState<string>("")
+    const [password, setPassword] = useState<string>("")
+    const [age, setAge] = useState<string>("")
+    const [gender, setGender] = useState<string>("")
+    const [genderPreference, setGenderPreference] = useState<string>("")
+  
+    const loading = false
+  
+    return (
+      <form className="space-y-6">
+        <div>
+          <label htmlFor="name" className="block-text-sm font-medium text-gray-700">
+            Name
+          </label>
+          <div className="mt-1">
+            <input 
+              id='name'
+              name='name'
+              type='name'
+              autoComplete='name'
+              required
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-pink-500 focus:boder-pink-500 sm:text-sm'
+            />
+          </div>
+        </div>
+  
+        <div>
+          <label htmlFor="email" className="block-text-sm font-medium text-gray-700">
+            Email Address
+          </label>
+          <div className="mt-1">
+            <input 
+              id='email'
+              name='email'
+              type='email'
+              autoComplete='email'
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-pink-500 focus:boder-pink-500 sm:text-sm'
+            />
+          </div>
+        </div>
+  
+        <div>
+          <label htmlFor="password" className="block-text-sm font-medium text-gray-700">
+            Password
+          </label>
+          <div className="mt-1">
+            <input 
+              id='password'
+              name='password'
+              type='password'
+              autoComplete='password'
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-pink-500 focus:boder-pink-500 sm:text-sm'
+            />
+          </div>
+        </div>
+
+        <div>
+          <label htmlFor="age" className="block-text-sm font-medium text-gray-700">
+            Age
+          </label>
+          <div className="mt-1">
+            <input 
+              id='age'
+              name='age'
+              type='age'
+              autoComplete='age'
+              required
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
+              min='18'
+              max='120'
+              className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-pink-500 focus:boder-pink-500 sm:text-sm'
+            />
+          </div>
+        </div>
+
+        <div>
+          <label className="block text-sm font-medium text-gray-700">Your Gender</label>
+          <div className="mt-2 flex gap-2">
+            <div className="flex items-center">
+              <input 
+                id='male'
+                name='gender'
+                type='checkbox'
+                checked={gender === "male"}
+                onChange={() => setGender("male")}
+                className='h-4 w-4 text-pink-600 focus:ring-pink-600 border-gray-300 rounded'
+              />
+              <label htmlFor="male" className='ml-2 block text-sm text-gray-900'>Male</label>
+            </div>
+            <div className="flex items-center">
+              <input 
+                id='female'
+                name='gender'
+                type='checkbox'
+                checked={gender === "female"}
+                onChange={() => setGender("female")}
+                className='h-4 w-4 text-pink-600 focus:ring-pink-600 border-gray-300 rounded'
+              />
+              <label htmlFor="female" className='ml-2 block text-sm text-gray-900'>Female</label>
+            </div>
+          </div>
+        </div>
+  
+        <div>
+          <label className="block text-sm font-medium text-gray-700">Gender Preferences</label>
+          <div className="mt-2 flex gap-2">
+            <div className="flex items-center">
+              <input 
+                id='prefer-male'
+                name='gender-preference'
+                type='radio'
+                checked={genderPreference === "male"}
+                onChange={() => setGenderPreference("male")}
+                className='h-4 w-4 text-pink-600 focus:ring-pink-600 border-gray-300 '
+              />
+              <label htmlFor="prefer-male" className='ml-2 block text-sm text-gray-900'>Male</label>
+            </div>
+            <div className="flex items-center">
+              <input 
+                id='prefer-female'
+                name='gender-preference'
+                type='radio'
+                checked={genderPreference === "female"}
+                onChange={() => setGenderPreference("female")}
+                className='h-4 w-4 text-pink-600 focus:ring-pink-600 border-gray-300 rounded'
+              />
+              <label htmlFor="prefer-female" className='ml-2 block text-sm text-gray-900'>Female</label>
+            </div>
+            <div className="flex items-center">
+              <input 
+                id='prefer-both'
+                name='gender-preference'
+                type='radio'
+                checked={genderPreference === "both"}
+                onChange={() => setGenderPreference("both")}
+                className='h-4 w-4 text-pink-600 focus:ring-pink-600 border-gray-300 rounded'
+              />
+              <label htmlFor="prefer-both" className='ml-2 block text-sm text-gray-900'>Both</label>
+            </div>
+          </div>
+        </div>
+  
+        <button type='submit' disabled={loading}
+          className={`w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white ${
+              loading 
+                      ? "bg-pink-400 cursor-not-allowed"
+                      : "bg-pink-600 hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"
+          }`}
+        >
+          { loading ? "Signing In..." : "Sign In" }
+        </button>
+      </form>
+    )
+}
+
+export default SignUpForm
--- client/src/pages/Auth.tsx
+++ client/src/pages/Auth.tsx
@@ -1,6 +1,31 @@
+import { useState } from "react"
+import LoginForm from "../components/LoginForm"
+import SignUpForm from "../components/SignUpForm"
+
 const Auth = () => {
+  const [isLogin, setIsLogin] = useState<boolean>(true)
+
   return (
-    <div>Auth</div>
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-red-500 to-pink-500 p-4">
+      <div className="w-full max-w-md">
+        <h2 className="text-center text-3xl font-extrabold text-white mb-8">
+          { isLogin ? "Sign in to Flick" : "Create a Flick Account" }
+        </h2>
+        <div className="bg-white shadow-xl rounded-lg p-8">
+          { isLogin ? <LoginForm /> : <SignUpForm /> }
+          <div className="mt-8 text-center">
+            <p className="text-sm text-gray-800">
+              { isLogin ? "New to Flick?" : "Already have an account?" }
+            </p>
+            <button onClick={() => setIsLogin((prevIsLogin) => !prevIsLogin)}
+              className="mt-2 text-red-600 hover:text-red-800 font-medium transition-colors duration-300"
+            >
+              { isLogin ? "Create a new account" : "Sign in to your account" }
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
